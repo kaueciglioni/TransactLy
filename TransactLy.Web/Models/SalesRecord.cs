456e5241e4927d2f2e310d155fbe9d5774e77354
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2025-12-11 01:48:05 -0300

    Implementing grouping search

--- TransactLy.Web/Models/SalesRecord.cs
+++ TransactLy.Web/Models/SalesRecord.cs
@@ -1,11 +1,16 @@
-﻿using TransactLy.Web.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TransactLy.Web.Models.Enums;
 
 namespace TransactLy.Web.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
--- TransactLy.Web/Services/SalesRecordsService.cs
+++ TransactLy.Web/Services/SalesRecordsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using TransactLy.Web.Data;
 using TransactLy.Web.Models;
 
@@ -31,6 +32,27 @@ namespace TransactLy.Web.Services
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where( x => x.Date >= minDate .Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where( x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
                 .ToListAsync();
         }
     }
